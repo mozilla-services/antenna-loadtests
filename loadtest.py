b35adf82bec23e0a68ac637f8f5d4b5fbbca499d
Matt Brandt <mbrandt@mozilla.com>
2016-11-21 16:28:00 -0700

    feat: add some tests

--- loadtest.py
+++ loadtest.py
@@ -1,43 +1,98 @@
 import os
-import uuid
 
-from ailoads.fmwk import scenario, requests
+from ailoads.fmwk import scenario
 
-from connection_handler import ConnectionHandler
+import utils
 
 URL_SERVER = os.getenv('URL_SERVER',
-                       'https://antenna.stage.mozaws.net')
+                       'http://antenna.stage.mozaws.net')
+URL_DMPFILES = 'https://s3-us-west-2.amazonaws.com/fx-test-antenna'
 DEBUG = True
 
 _LINE = '---------------------------------'
 _CONNECTIONS = {}
 
-PERCENTAGE = 100
-# curl -k  --data "mozstd-track-digest256;a:1" https://shavar.stage.mozaws.net/downloads # noqa
+# utils._log_everything()
 
 
 def log_header(msg):
     print('{0}\n{1}\n{0}'.format(_LINE, msg))
 
 
-def get_connection(id=None):
-    if id is None or id not in _CONNECTIONS:
-        id = uuid.uuid4().hex
-        conn = ConnectionHandler(id, URL_SERVER)
-        _CONNECTIONS[id] = conn
-
-    return _CONNECTIONS[id]
-
-
-@scenario(PERCENTAGE)
-def get_version():
+@scenario(0)
+def test_get_version():
     """Get version from Antenna server"""
-
     conn = get_connection('service-under-test')
-
-    if DEBUG:
-        log_header(list)
     resp = conn.get('/__version__')
     if DEBUG:
+        print(resp.status_code)
+        print(resp.headers)
         print(resp.text)
+    resp.raise_for_status()
+
+
+@scenario(0)
+def test_post_empty_dmp():
+    crash_payload = utils.assemble_crash_payload(
+        raw_crash={'ProductName': 'Firefox', 'Version': '1'},
+        dumps={}
+    )
+    resp = utils.post_crash(
+        url=URL_SERVER,
+        crash_payload=crash_payload)
+    print('test_post_empty_dmp: %s' % resp)
+    resp.raise_for_status()
+
+
+@scenario(100)
+def test_crash_400k_uncompressed():
+    size = (400 * 1024)
+    raw_crash, dumps = utils.generate_sized_crashes(size)
+    crash_payload = utils.assemble_crash_payload(raw_crash, dumps)
+    resp = utils.post_crash(URL_SERVER, crash_payload)
+    print("test_crash_400k_uncompressed: %s" % resp)
+    resp.raise_for_status()
+
+
+@scenario(0)
+def test_crash_4mb_uncompressed():
+    size = (4 * 1024 * 1024)
+    raw_crash, dumps = utils.generate_sized_crashes(size)
+    crash_payload = utils.assemble_crash_payload(raw_crash, dumps)
+    payload, headers = utils.multipart_encode(crash_payload)
+
+    if len(payload) != size:
+        raise ValueError('payload size %s', len(payload))
+
+    resp = utils.post_crash(URL_SERVER, crash_payload)
+    print("test_crash_4mb_uncompressed: %s" % resp)
+    resp.raise_for_status()
+
+
+@scenario(0)
+def test_crash_20mb_uncompressed():
+    size = (20 * 1024 * 1024)
+    raw_crash, dumps = utils.generate_sized_crashes(size)
+    crash_payload = utils.assemble_crash_payload(raw_crash, dumps)
+    payload, headers = utils.multipart_encode(crash_payload)
+
+    if len(payload) != size:
+        raise ValueError('payload size %s', len(payload))
+
+    resp = utils.post_crash(URL_SERVER, crash_payload)
+    print("test_crash_20mb_uncompressed: %s" % resp)
+    resp.raise_for_status()
+
+
+@scenario(0)
+def test_crash_greater_than_20mb_uncompressed():
+    size = (50 * 1024 * 1024)
+    raw_crash, dumps = utils.generate_sized_crashes(size)
+    crash_payload = utils.assemble_crash_payload(raw_crash, dumps)
+    payload, headers = utils.multipart_encode(crash_payload)
+
+    if len(payload) != size:
+        raise ValueError('payload size %s', len(payload))
+    resp = utils.post_crash(URL_SERVER, crash_payload)
+    print("test_crash_greater_than_20mb_uncompressed: %s" % resp)
     resp.raise_for_status()
